432087608e1df59b1616e53b319a25f484137cfc
saddammd <Saddam Hussain@localhost>
2021-07-08 15:11:09 +0530

    Mail error solved

---
+++ demo.user.management/src/main/java/com/ashok/it/demo/user/management/config/MailConfig.java
@@ -0,0 +1,18 @@
+package com.ashok.it.demo.user.management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+	
+	
+	    @Bean
+	    public JavaMailSender javaMailSender() { 
+	          return new JavaMailSenderImpl();
+	        		  
+	    }
+}
+
